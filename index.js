62448b98e738a951649c5ec6716669e60ae8fd90
Ignacio Yz <ignacioyz@gmail.com>
2022-05-16 21:30:26 -0400

    commit tree 50bb2e4220fb721387a2f059a648551cc3c9f450

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function receivesAFunction(callback){ 
+callback()
+}
+
+ function returnsANamedFunction(){
+   return function balls(){ 
+   }
+ }
+
+ function returnsAnAnonymousFunction(){
+  return function(){ 
+  }
+ }
